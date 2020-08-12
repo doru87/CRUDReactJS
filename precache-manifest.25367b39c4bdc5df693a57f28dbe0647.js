8f47dfa874581e75a9163a6a83502188ed349ee2
doru87 <32599645+doru87@users.noreply.github.com>
2020-08-12 13:03:45 +0300

    Updates

---
+++ precache-manifest.25367b39c4bdc5df693a57f28dbe0647.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd7411664bb6ea639066852d16e987e",
+    "url": "/CRUDReactJS/index.html"
+  },
+  {
+    "revision": "e75c471909ddbe8e568d",
+    "url": "/CRUDReactJS/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "092a39312c41aa3aecfd",
+    "url": "/CRUDReactJS/static/css/main.558fc198.chunk.css"
+  },
+  {
+    "revision": "e75c471909ddbe8e568d",
+    "url": "/CRUDReactJS/static/js/2.f862b848.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CRUDReactJS/static/js/2.f862b848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092a39312c41aa3aecfd",
+    "url": "/CRUDReactJS/static/js/main.2cab5c4c.chunk.js"
+  },
+  {
+    "revision": "77f9c4974167361d843f",
+    "url": "/CRUDReactJS/static/js/runtime-main.79494e46.js"
+  }
+]);
